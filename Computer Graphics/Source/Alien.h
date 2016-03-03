87fc14cbf0a5b54013438fa4c4e8e8ac0d2fe384
CraliX <wayneaw123@Hotmail.com>
2016-03-03 09:58:01 +0800

    DOcumentation for medic , alien and alienememy

--- Computer Graphics/Source/Alien.h
+++ Computer Graphics/Source/Alien.h
@@ -1,7 +1,25 @@
+/******************************************************************************/
+/*!
+\file	Alien.h
+\author Wayne Aw
+\par	Email: 152558X@mymail.nyp.edu.sg
+\par	Admin Number: 152558X
+\par	School: Nanyang Polytechnic (School of Interactive and Digital Media)
+\par	Diploma: Game Development and Technology
+\par	Class: GD1504
+\brief
+Class to define alien while inheriting from GameChar
+*/
+/******************************************************************************/
 #ifndef ALIEN_H
 #define ALIEN_H
 #include "GameChar.h"
 
+/******************************************************************************/
+/*!
+Class Alien:
+\brief	Providing the AlienEnemies to move ( inherits from Alien )
+/******************************************************************************/
 class Alien : public GameChar
 {
 public:
--- Computer Graphics/Source/AlienEnemy.cpp
+++ Computer Graphics/Source/AlienEnemy.cpp
@@ -1,3 +1,16 @@
+/******************************************************************************/
+/*!
+\file	AlienEnemy.h
+\author Wayne Aw
+\par	Email: 152558X@mymail.nyp.edu.sg
+\par	Admin Number: 152558X
+\par	School: Nanyang Polytechnic (School of Interactive and Digital Media)
+\par	Diploma: Game Development and Technology
+\par	Class: GD1504
+\brief
+Functions of AlienEnemy
+*/
+/******************************************************************************/
 #include "AlienEnemy.h"
 
 /****************************************************************************/
@@ -13,15 +26,13 @@ AlienEnemy::AlienEnemy()
 /****************************************************************************/
 /*!
 \brief
- Logic Behind the AlienEnemies' movement 
-\param object_name 
-Point to calculate distance from
-\exception Divide by zero
-thrown if normalizing a zero vector
-\return
-Resulting normalized vector
-*/
-/****************************************************************************/
+Setting the speed and translation of up and down
+\param object_name - name of alien
+\param int boundary - boundary of the alien
+\param float viewAngle
+\param Vector3 pos - Appoints the xyz position of the alien
+
+/****************************************************************************/
 AlienEnemy::AlienEnemy(string object_name, int boundary, float viewAngle, Vector3 pos) : Alien(object_name, boundary, viewAngle, pos)
 {
 	turnSpeed = 45;
@@ -29,12 +40,23 @@ AlienEnemy::AlienEnemy(string object_name, int boundary, float viewAngle, Vector
 	wormtranslate = wormrotate = false;
 	wormtranslating = wormrotating = 0;
 }
-
+/******************************************************************************/
+/*!
+\brief
+AlienEnemy's default destructor decrements count
+*/
+/******************************************************************************/
 AlienEnemy::~AlienEnemy()
 {
 
 }
-
+/******************************************************************************/
+/*!
+\brief
+This makes the worm go up and down
+\param double dt - To be put in update()
+*/
+/******************************************************************************/
 void AlienEnemy::translateWorm(double dt)
 {
 	if (isTurned == false)
@@ -54,7 +76,13 @@ void AlienEnemy::translateWorm(double dt)
 		}
 	}
 }
-
+/******************************************************************************/
+/*!
+\brief
+This allows the worm to move forward to a specific location and then rotate 180 degrees to backtrack. (non - stop)
+\param double dt - To be put in update()
+*/
+/******************************************************************************/
 void AlienEnemy::move(double dt)
 {
 	if (wormtranslate == false)
